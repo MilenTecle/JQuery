77478c0d0ca5945a20c1011c466cf169d843a26a
Milen Tecle <m_tecle@hotmail.com>
2023-12-03 19:47:44 +0000

    Try different approaches

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 $(document).ready(function() {
-    $("#stream1_btn").on(function() {
-        $(".stream1").slideDown();
-        $(".stream1").slideDown('slow');
-        $(".stream1").hide('medium');
-        $(".stream1").hide('fast');
-        $(".stream1").hide(1000);
+    $(".box").on("click", function() {
+        v/**
+    	 * When we click on an element that has
+    	 * a 'box' class, this event will be fired.
+    	 */
+    	var classNames = $(this).attr("class").split(" ");
+    	var boxClass = classNames[0];
+    	var className = classNames[1];
+    	if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+    		// If this object is already red, turn it black
+    		$("." + className).css("background-color", "#000");
+    	} else {
+    		// Else turn all elements with a box class black
+    		// and then change the color of all elements
+    		// with the same class name as the clicked element
+    		// to red.
+    		$("." + boxClass).css("background-color", "#000");
+    		$("." + className).css("background-color", "red");
+    	}
     });
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
-   });
-   $("#stream2_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream2").addClass('highlight_stream');
-   });
-   $("#stream3_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream3").addClass('highlight_stream');
-   });
+})
